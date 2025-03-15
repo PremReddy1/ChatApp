3620173ab120c8f3a86d283dfd85e9e0f4e013c2
PremReddy1 <premkumarkalpal@gmail.com>
2025-03-15 13:40:24 +0530

    update UI

--- server.js
+++ server.js
@@ -1,44 +1,30 @@
-const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-const PORT = 3000;
+app.use(express.static("public"));
 
-app.use(express.static('public'));
+io.on("connection", (socket) => {
+  console.log("New user connected");
 
-io.on('connection', (socket) => {
-    console.log('A user connected');
+  socket.on("join", (username) => {
+    socket.username = username;
+    console.log(`${username} joined`);
+  });
 
-    // Listen for user joining with a name
-    socket.on('set username', (username) => {
-        socket.username = username;
-        console.log(`${username} joined the chat`);
-        io.emit('user joined', `${username} joined the chat`);
-    });
+  socket.on("chatMessage", ({ username, message, timestamp }) => {
+    io.emit("message", { username, message, timestamp });
+  });
 
-    // Listen for messages from clients
-    socket.on('chat message', (msg) => {
-        const timestamp = new Date().toLocaleTimeString();
-        const messageData = {
-            username: socket.username || 'Anonymous',
-            message: msg,
-            time: timestamp
-        };
-        io.emit('chat message', messageData);
-    });
-
-    socket.on('disconnect', () => {
-        if (socket.username) {
-            io.emit('user left', `${socket.username} left the chat`);
-        }
-        console.log('A user disconnected');
-    });
+  socket.on("disconnect", () => {
+    console.log(`${socket.username} disconnected`);
+  });
 });
 
-server.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+server.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
 });
